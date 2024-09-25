4471dc0c71fa43f6d74993e8efa679cdc7261583
suudi abdisalan <abdisalansucdi@gmail.com>
2024-09-25 16:51:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+  const hqStreet = 42;
+  return Math.abs(street - hqStreet)
+}
+
+ function distanceFromHqInFeet(street){
+  let hqStreet = 42;
+  const feetPerStreet = 264; 
+  if(street < hqStreet){
+    return (hqStreet - street) * feetPerStreet;
+  }
+  else {
+    return (street - hqStreet) * feetPerStreet;
+  }
+ }
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination - start ) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+      if(distance <= 400){
+        return 0;
+      }
+      else if (distance > 400 && distance <= 2000){
+return (distance - 400) * 0.02;
+      }
+      else if (distance > 2000 && distance <= 2500){
+        return 25;
+      }
+      else{
+        return "cannot travel that far";
+      }
+}
+  
+  
+  
+  
+  
+  
